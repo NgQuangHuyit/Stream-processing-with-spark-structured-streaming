45b6d384a6570ec7919c5c29a38b3edb7040a4b1
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2024-08-30 12:05:34 +0700

    fix bug inconsistency schemacon

--- applications/KafkaAvroSink/KafkaAvroSink.py
+++ applications/KafkaAvroSink/KafkaAvroSink.py
@@ -54,11 +54,11 @@ if __name__ == "__main__":
         .load()
 
     value_df = kafka_df.select(from_json(col("value").cast("string"), schema).alias("value"))
-    exploded_df = value_df.selectExpr("value.InvoiceNumber", "value.CreatedTime", "value.PosID",
-                                      "value.CustomerType", "value.PaymentMethod", "value.DeliveryType",
-                                      "value.DeliveryAddress.City",
-                                      "value.DeliveryAddress.State", "value.DeliveryAddress.PinCode",
-                                      "explode(value.InvoiceLineItems) as LineItem")
+    exploded_df = value_df.selectExpr("value.InvoiceNumber", "value.CreatedTime", "value.StoreID",
+                                     "value.PosID", "value.CustomerType", "value.CustomerCardNo", "value.DeliveryType",
+                                     "value.DeliveryAddress.City",
+                                     "value.DeliveryAddress.State", "value.DeliveryAddress.PinCode",
+                                     "explode(value.InvoiceLineItems) as LineItem")
 
     flattened_df = exploded_df.withColumn("ItemCode", expr("LineItem.ItemCode")) \
         .withColumn("ItemDescription", expr("LineItem.ItemDescription")) \
